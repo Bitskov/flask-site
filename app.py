f58abffb22ebe1a559f2100c48f19131cac0d67c
Alexei Bykov <bitskoff@gmail.com>
2019-01-23 15:34:22 +0800

    Added Index page prototype

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -9,4 +10,26 @@ def index():
     return render_template('index.html')
 
 
-app.run(port=8080)
+@app.route('/sign-in')
+
+
+#
+# CSS PROCESSING
+#
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+
+app.run(host='192.168.1.6', port=8080, debug=True)
